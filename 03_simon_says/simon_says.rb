90cf6f7c9bb3b7d24a20365b2e4ec423032364bb
david yu <davidfyu@umich.edu>
2020-02-12 22:54:25 -0500

    pass 03_simon_says testcases

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
-#write your code here
+def echo(x)
+  return x
+end
+
+def shout(x)
+  return x.upcase
+end
+
+def repeat(x, y=2)
+  z = x
+  for i in 1..y-1 do
+    z = z + " " + x
+  end
+  return z
+end
+
+def start_of_word(x, y)
+  return x[0..y-1]
+end
+
+def first_word(x)
+  return x.split(' ')[0]
+end
+
+def titleize(x)
+  a = x.split(' ')
+  ret = ""
+  for i in a
+    ret = ret + i.capitalize + " "
+  end
+  return ret[0..-2]
+end
+
